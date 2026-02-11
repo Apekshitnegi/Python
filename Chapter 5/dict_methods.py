73091209b50981c5de4a82c5daf0b59a2d460966
Apekshitnegi <apekshitnegi9@gmail.com>
2026-02-11 18:40:35 +0530

    Create dict_methods.py

---
+++ Chapter 5/dict_methods.py
@@ -0,0 +1,11 @@
+Marks = {
+    "lucky": 55,
+    "Negi": 30,
+    "bkl" : 40
+}
+
+#print(Marks.items())
+#print(Marks.keys())
+#print(Marks.values())
+Marks.update({"Negi": 30 , "Renuka": 100})
+print(Marks)
