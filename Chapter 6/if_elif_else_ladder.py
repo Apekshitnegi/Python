2e6110fe57bfcd2d4319507bb7be209d803aa1d4
Apekshitnegi <apekshitnegi9@gmail.com>
2026-06-23 01:11:13 +0530

    Create if_elif_else_ladder.py

---
+++ Chapter 6/if_elif_else_ladder.py
@@ -0,0 +1,18 @@
+a = int(input("Enter your age: "))
+
+# If elif else ladder
+if(a>=18):
+    print("You are above the age of consent................")
+    print("Good for you...............")
+
+elif(a<0):
+   print("you are entering invailed age............")
+
+elif(a==0):
+   print("you entering 0 which is not vailed age.........")
+
+else:
+    print("You are below the age of consent.......")
+
+
+print("End of Program................")
