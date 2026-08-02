244da1b1a1972c46a3bf5fa8ff829f261885e760
Apekshitnegi <apekshitnegi9@gmail.com>
2026-08-02 19:19:07 +0530

    Create 04_problem.py

---
+++ Chapter 9/Practice question/04_problem.py
@@ -0,0 +1,9 @@
+word = "Donkey" 
+
+with open("text.txt", "r") as f:
+    content = f.read()
+
+contentNew = content.replace(word, "####")
+
+with open(rf"C:\Users\Apekshit negi\OneDrive\Documents\Desktop\Python\Chapter 9\Practice question\text.txt", "w") as f:
+    f.write(contentNew)
